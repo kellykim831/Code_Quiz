9df0002b09972e8cfc2440826de59a27bdb125f3
Kelly Kim <kellykim408@gmail.com>
2020-09-22 21:57:14 -0700

    create variables in script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+var startButton = document.querySelector('.start');
+var quiz = document.querySelector('.quiz');
+var rules = document.querySelector('.rules');
+var answer = document.getElementById('answer');
+var timer;
+
+// startButton.addEventListener('click', function(){
+//     rules.style.display = 'none';
+//     startButton.style.display = 'none';
+//     quiz.style.display = 'block';
+//     quizTimer();
+
+// });
+
+// function showHideQuiz(){
+//     quiz.style.display = 'none';
+// }
+
+// function quizTimer() {
+//     timer = setTimeout(function () {
+//         quiz.style.display = 'none';
+//         answer.innerHTML = <p>You did not answer</p>;
+//     }, 3000);
+
+// }
+
+// showHideQuiz();
