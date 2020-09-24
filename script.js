748738f3d3c6620c6d9990fcc23b7283c78ce500
Kelly Kim <kellykim408@gmail.com>
2020-09-23 21:07:36 -0700

    create class for line items on multiple choice

--- script.js
+++ script.js
@@ -2,26 +2,36 @@ var startButton = document.querySelector('.start');
 var quiz = document.querySelector('.quiz');
 var rules = document.querySelector('.rules');
 var answer = document.getElementById('answer');
+var question = document.querySelector('#question');
+var question2 = document.querySelector('#question2');
+var answerChoice = document.querySelector('.choice');
 var timer;
 
-// startButton.addEventListener('click', function(){
-//     rules.style.display = 'none';
-//     startButton.style.display = 'none';
-//     quiz.style.display = 'block';
-//     quizTimer();
+startButton.addEventListener('click', function(){
+    rules.style.display = 'none';
+    startButton.style.display = 'none';
+    quiz.style.display = 'block';
+    quizTimer();
 
-// });
+});
 
-// function showHideQuiz(){
-//     quiz.style.display = 'none';
-// }
+answerChoice.addEventListener('click', function(){
+    question.style.display = 'none';
+    question2.style.display = 'block';
+    quizTimer();
+    console.log(answerChoice.length);
+});
 
-// function quizTimer() {
-//     timer = setTimeout(function () {
-//         quiz.style.display = 'none';
-//         answer.innerHTML = <p>You did not answer</p>;
-//     }, 3000);
+function showHideQuiz(){
+    quiz.style.display = 'none';
+}
 
-// }
+function quizTimer() {
+    timer = setTimeout(function () {
+        quiz.style.display = 'none';
+        answer.innerHTML = "You did not answer";
+    }, 120000);
 
-// showHideQuiz();
+}
+
+showHideQuiz();
